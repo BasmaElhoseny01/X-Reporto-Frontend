ec5f8a0008c9b6c67c1e86eda950d87d422d19e1
BasmaElhoseny <basmaelhoseny6@gmail.com>
2024-06-15 22:15:25 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,12 @@
 // import { Header } from "antd/es/layout/layout";
 import { Button } from "antd";
 import React from "react";
-import Button from "../components/common/Button/Button";
-// import { actionsCreators,MainState } from "../state";
-// import { useDispatch, useSelector } from "react-redux";
-// import { bindActionCreators } from "redux";
 
-interface HomeProps {
-  // Define props here
-}
+// interface HomeProps {
+//   // Define props here
+// }
 
-const Home: React.FC<HomeProps> = ({}) => {
+function Home() {
   return (
     <div>
       <Button>basma</Button>
