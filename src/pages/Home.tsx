a229472fd4d58e37aa148a923ec772a11c72c94f
BasmaElhoseny <basmaelhoseny6@gmail.com>
2024-06-16 22:31:07 +0300

    Feat: Added Messages Buttons Examples

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,12 +6,52 @@ import SecondaryButton from "../components/common/SecondaryButton/SecondaryButto
 
 // Ant Design
 import { SearchOutlined, DeleteOutlined } from "@ant-design/icons";
+import { message } from "antd";
 import GeneralTable from "../components/common/Table/Table";
 
 function Home() {
- 
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const success = () => {
+    messageApi.open({
+      type: "success",
+      content: "This is a success message",
+    });
+  };
+
+  const error = () => {
+    messageApi.open({
+      type: "error",
+      content: "This is an error message",
+    });
+  };
+
+  const warning = () => {
+    messageApi.open({
+      type: "warning",
+      content: "This is a warning message",
+    });
+  };
+
+  const info = () => {
+    messageApi.open({
+      type: "info",
+      content: "This is an info message",
+    });
+  };
+
+  const loading = () => {
+    messageApi.open({
+      type: "loading",
+      content: "Action in progress..",
+      duration: 0,
+    });
+    // Dismiss manually and asynchronously
+    setTimeout(messageApi.destroy, 2500);
+  };
+
   // for testing
-  const GeneralTableData={
+  const GeneralTableData = {
     columns: [
       {
         title: "ID",
@@ -35,18 +75,20 @@ function Home() {
     api: "https://jsonplaceholder.typicode.com/todos",
 
     // eslint-disable-next-line
-    action: (record:any, rowIndex:any) => {
+    action: (record: any, rowIndex: any) => {
       console.log(record, rowIndex);
     },
   };
   return (
     <div>
+      {contextHolder}
       <div
         style={{
           // height: "500px",
           display: "flex",
           flexDirection: "row",
           justifyContent: "space-around",
+          flexWrap: "wrap",
           padding: "2rem",
         }}
       >
@@ -65,8 +107,22 @@ function Home() {
 
         <PrimaryButton icon={<SearchOutlined />}>Basma</PrimaryButton>
         <SecondaryButton icon={<SearchOutlined />}>Basma</SecondaryButton>
+
+        <PrimaryButton loading>Basma</PrimaryButton>
+        <SecondaryButton loading>Basma</SecondaryButton>
+
+        {/* Messages */}
+        <PrimaryButton onClick={success}>Success</PrimaryButton>
+        <PrimaryButton onClick={error}>Error</PrimaryButton>
+        <PrimaryButton onClick={warning}>Warn</PrimaryButton>
+        <PrimaryButton onClick={info}>Info</PrimaryButton>
+        <PrimaryButton onClick={loading}>Loading</PrimaryButton>
       </div>
-      <GeneralTable columns={GeneralTableData.columns} api={GeneralTableData.api} action={GeneralTableData.action}/>
+      <GeneralTable
+        columns={GeneralTableData.columns}
+        api={GeneralTableData.api}
+        action={GeneralTableData.action}
+      />
     </div>
   );
 }
