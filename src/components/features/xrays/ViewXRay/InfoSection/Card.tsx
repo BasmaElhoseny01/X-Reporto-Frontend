0d06525ae0e3c13b0b779145e4f2a8fe9a5cdfe8
AhmedHosny2024 <eng.ahmedhosny2024@gmail.com>
2024-07-08 00:28:38 +0300

    integrate image endpoint

--- src/components/features/xrays/ViewXRay/InfoSection/Card.tsx
+++ src/components/features/xrays/ViewXRay/InfoSection/Card.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CardContainer, CardDate, CardSecondSubContainer, CardSubContainer, ImageDetails, SubTitle } from "./InfoSection.Styles";
 import LinkButton from "../../../../common/LinkButton/LinkButton";
 import { Image } from "antd";
 import {FolderOpenFilled} from "@ant-design/icons";
+import { useSelector } from "react-redux";
+import { MainState } from "../../../../../state";
+import axios from "../../../../../services/apiService";
 
 type CardProps = {
   id:number;
@@ -11,11 +14,35 @@ type CardProps = {
   status:string;
 };
 function Card(props: CardProps) {
-  console.log(process.env.REACT_APP_BASE_URL + "/" + props.xray_path);
+  const [imageUrl, setImageUrl] = useState<string>('');
+  const token = useSelector((state: MainState) => state.token);
+
+  useEffect(() => {
+    const fetchImageUrl = async () => {
+       axios.get(`${process.env.REACT_APP_API_URL_DEV}/api/v1/results/download_file?file_path=${props.xray_path}`, {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        }).then((response) => {
+        if (response.status === 200) {
+          console.log(response);
+          const blob = response.data.blob();
+          const url = URL.createObjectURL(blob);
+          setImageUrl(url);
+        } else {
+          console.error('Failed to fetch image');
+        }
+      }).catch((error) => {
+        console.error('Error fetching image: ', error);
+      });
+    };
+    fetchImageUrl();
+  }, [props.xray_path]);
+
   return (
     <CardContainer>
         <CardSubContainer>
-          <Image width={40} src={process.env.REACT_APP_BASE_URL + "/download/" + props.xray_path} />
+          <Image width={40} src={imageUrl} />
           <ImageDetails>
             <LinkButton onClick={()=>window.location.pathname=`/reports/${props.id}`}>{props.id}</LinkButton>
             <CardDate>Created at : {props.created_at}</CardDate>
