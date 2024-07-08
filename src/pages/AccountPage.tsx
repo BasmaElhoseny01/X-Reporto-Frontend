102711750d48a800d9cf3f70c7a864a88a7157e8
zeinabmoawad <92188433+zeinabmoawad@users.noreply.github.com>
2024-07-08 22:03:24 +0300

    add account page

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import ViewEmployee from "../components/features/employee/ViewEmployee/ViewEmployee";
+import { useSelector } from "react-redux";
+import { MainState } from "../state/Reducers";
 function AccountPage() {
-  return <div>AccountPage</div>;
+  const user = useSelector((state: MainState) => state.user);
+  const userType = user?.type || ""; // Set a default value for userType if it's undefined
+
+  return <div>
+    <ViewEmployee type={userType} />
+  </div>;
 }
 
 export default AccountPage;
